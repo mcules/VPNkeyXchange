1754a5ba3fbf3585921f3c7aa7ee6e8fe20e25ac
Tobias Klaus <tk@meskal.net>
2017-10-05 18:32:33 +0200

    keyxchangev2: inital commit
    From: Tim Niemeyer <tim@tn-x.org>
    From: ChristianDr <fff@chrisi01.de>

--- index.php
+++ index.php
@@ -5,47 +5,85 @@
  * @author RedDog <reddog@mastersword.de>
  * @author delphiN <freifunk@wunschik.net>
  * @author Mose <mose@fabfolk.com>
+ * @author ChristianDr <fff@chrisi01.de>
  *
  * @license https://www.gnu.org/licenses/agpl-3.0.txt AGPL-3.0
  */
-const DEFAULT_HOOD_ID = 0;
-const INVALID_MAC = 'AAAAAAAAAAAA';
 const DEBUG = false;
+
+const hood_mysql_fields = 'ID,
+name,
+ESSID_AP as essid,
+BSSID_MESH as mesh_bssid,
+ESSID_MESH as mesh_essid,
+protocol,
+channel2,
+mode2,
+type2,
+channel5,
+mode5,
+type5,
+upgrade_path,
+ntp_ip,
+UNIX_TIMESTAMP(changedOn) as timestamp,
+prefix, lat, lon';
+
+function debug($msg)
+{
+  if (DEBUG)
+  {
+    print_r($msg);
+    echo "\n";
+  }
+}
 
 /**
  * Singelton DB instance
  */
-class db{
+class db
+{
   private static $instance = NULL;
-  private function __construct(){
+
+  private function __construct()
+  {
   }
-  public static function getInstance(){
-    if(!self::$instance){
+  
+  public static function getInstance()
+  {
+    if(!self::$instance)
+    {
       require('config.inc.php');
       self::$instance = new PDO('mysql:host='.$mysql_server.';dbname='.$mysql_db,$mysql_user,$mysql_pass);
       self::$instance->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     }
     return self::$instance;
   }
-  private function __clone(){
+
+  private function __clone()
+  {
   }
 }
 
 /**
  * returns details error msg (as json)
  *
- * @param integer $code
- *          HTTP error 400, 500 or 503
- * @param string $msg
- *          Error message text
+ * @param integer $code HTTP error 400, 500 or 503
+ * @param string $msg Error message text
  */
-function showError($code,$msg){
+function showError($code,$msg)
+{
   if($code == 400)
+  {
     header('HTTP/1.0 400 Bad Request');
+  }
   elseif($code == 500)
+  {
     header('HTTP/1.0 500 Internal Server Error');
+  }
   elseif($code == 503)
+  {
     header('HTTP/1.0 503 Service Unavailable');
+  }
 
   header('Content-Type: application/json');
 
@@ -53,149 +91,164 @@ function showError($code,$msg){
   print_r(json_encode($errorObject));
 }
 
-function debug($msg){
-  if(DEBUG)
-    print_r($msg."\n");
+function sin_d($value)
+{
+  return sin(deg2rad($value));
 }
 
-// ----------------------------------------------------------------------------
+function cos_d($value)
+{
+  return cos(deg2rad($value));
+}
 
-// get parameters and initialice settings
-if(isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'])
-  $ip = $_SERVER ['HTTP_X_FORWARDED_FOR'];
+const EARTH_RADIUS = 6371;
+/**
+ * Haversine distance function in km
+ * https://en.wikipedia.org/wiki/Haversine_formula
+ *
+ * @param double $lat1 latitude point 1
+ * @param double $lon1 longitude point 1
+ * @param double $lat2 latitude point 2
+ * @param double $lon2 longitude point 2
+ * @return integer distance between the points in km
+ */
+function distance_haversine($lat1,$lon1,$lat2,$lon2)
+{
+  $delta_lat = $lat1 - $lat2;
+  $delta_lon = $lon1 - $lon2;
+  $alpha = $delta_lat / 2;
+  $sin_alpha_2 = sin_d($alpha) * sin_d($alpha);
+  $beta = $delta_lon / 2;
+  $sin_beta_2 = sin_d($beta) * sin_d($beta);
+  $a = $sin_alpha_2 + cos_d($lat1) * cos_d($lat2) * $sin_beta_2;
+  $c = asin(min(1, sqrt($a)));
+  $distance = 2 * EARTH_RADIUS * $c;
+  $distance = round($distance, 3);
+  return $distance;
+}
 
-if(isset($_GET['mac']) && $_GET['mac'])
-  $mac = $_GET ['mac'];
-else
-  $mac = INVALID_MAC;
+/**
+ * Check is the given geo coordinates are within one of the hoods.
+ *
+ * @param double $lat latitude point 1
+ * @param double $lon longitude point 1
+ * @return integer hood-id
+ */
+function getHoodByGeo($lat,$lon)
+{
+  $current_hood_dist=99999999;
+  $best_result = array();
 
-if(isset($_GET['name']) && $_GET['name'])
-  $name = $_GET ['name'];
-
-if(isset($_GET['key']) && $_GET['key'])
-  $key = $_GET ['key'];
-
-if(isset($_GET['port']) && $_GET['port'])
-  $port = $_GET ['port'];
-else
-  $port = 10000;
-
-$hood = DEFAULT_HOOD_ID;
-
-// insert or update the current node in the database
-if(isset($ip) && $ip && isset($name) && $name && isset($key) && $key) {
-  if(!preg_match('/^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$/',$name))
-    exit(showError(400,'invalid name'));
-
-  if($mac != INVALID_MAC)
-    $sql = 'SELECT * FROM nodes WHERE mac=:mac;';
-  else
-    $sql = 'SELECT * FROM nodes WHERE (mac=\'000000000000\' OR mac=\''.INVALID_MAC.'\') AND (name=:name);';
-
-  try{
-    $rs = db::getInstance()->prepare($sql);
-
-    if($mac != INVALID_MAC)
-      $rs->bindParam(':mac',$mac);
-    else
-      $rs->bindParam(':name',$name);
-
-    $rs->execute ();
+  // load hoods from DB
+  try
+  {
+    $q = 'SELECT '.hood_mysql_fields.' FROM hoods;';
+    $rs = db::getInstance()->prepare($q);
+    $rs->execute();
   }
-  catch(PDOException $e) {
+  catch(PDOException $e)
+  {
     exit(showError(500,$e));
   }
 
-  if($rs->rowCount() > 1)
-    exit(showError(500,'To much nodes with mac='.$mac.', name='.$name));
+  // check for every hood if it's nearer than the hood before
+  while($result = $rs->fetch(PDO::FETCH_ASSOC))
+  {
+    debug("\n\nhood: ".$result['name']);
 
-  if($rs->rowCount() == 1){
-    $result = $rs->fetch(PDO::FETCH_ASSOC);
+    if(is_null($result['lat']) || is_null($result['lon']))
+    {
+      continue;
+    }
 
-    $hood = $result['hood_ID'];
+    debug('hoodCenterLat: '.$result['lat'].', hoodCenterLon: '.$result['lon'].', hoodID: '.$result['ID']);
 
-    if (!$result['readonly'] && !$result['notrain']) {
-      $updateHood=false;
-      if (!$result['isgateway']) {
-        if ($result['hood_ID'] != 0) {
-          $updateHood=true;
-          $hood = 0;
-        }
-      }
+    $distance = distance_haversine($result['lat'],$result['lon'],$lat,$lon);
+    debug('distance: $distance');
 
-      if ($updateHood)
-        $sql = 'UPDATE nodes SET ip=:ip, mac=:mac, name=:name, `key`=:key, port=:port, timestamp=CURRENT_TIMESTAMP, hood_ID=:hood WHERE ID=:id';
-      else
-        $sql = 'UPDATE nodes SET ip=:ip, mac=:mac, name=:name, `key`=:key, port=:port, timestamp=CURRENT_TIMESTAMP WHERE ID=:id';
-      try{
-        $rs = db::getInstance()->prepare($sql);
-        $rs->bindParam(':id',$result['ID'],PDO::PARAM_INT);
-        $rs->bindParam(':ip',$ip);
-        $rs->bindParam(':mac',$mac);
-        $rs->bindParam(':name',$name);
-        $rs->bindParam(':key',$key);
-        $rs->bindParam(':port',$port);
-        if ($updateHood)
-          $rs->bindParam(':hood',$hood);
-        $rs->execute();
-      }
-      catch(PDOException $e) {
-        exit(showError(500,$e));
-      }
+    if ($distance <= $current_hood_dist)
+    {
+      debug('Node belongs to Hood '.$result['ID'].'('.$result['name'].')');
+      $current_hood_dist = $distance;
+      $best_result = $result;
     }
   }
-  else{
-    $sql = 'INSERT INTO nodes(ip,mac,name,`key`,port,readonly,isgateway,hood_ID) VALUES (:ip,:mac,:name,:key,:port,0,0,:hood);';
-    try{
-      $rs = db::getInstance()->prepare($sql);
-      $rs->bindParam(':ip',$ip);
-      $rs->bindParam(':mac',$mac);
-      $rs->bindParam(':name',$name);
-      $rs->bindParam(':key',$key);
-      $rs->bindParam(':port',$port);
-      $rs->bindParam(':hood',$hood);
-      $rs->execute ();
-    }
-    catch(PDOException $e) {
-      exit(showError(500,$e));
-    }
+
+  return $best_result;
+}
+
+function getTrainstation()
+{
+  try
+  {
+    $q = 'SELECT '.hood_mysql_fields.' FROM hoods WHERE ID="0";';
+    $rs = db::getInstance()->prepare($q);
+    $rs->execute();
+  }
+  catch (PDOException $e)
+  {
+    exit(showError(500,$e));
+  }
+
+  return $rs->fetch(PDO::FETCH_ASSOC);
+}
+
+function getAllVPNs($hoodId)
+{
+  $ret = array();
+
+  // return either all all gateways from the hood
+  try
+  {
+    $sql = 'SELECT g.name, "fastd" AS protocol, g.ip AS address, g.port, g.key
+            FROM gateways AS g WHERE hood_ID=:hood;';
+    $rs = db::getInstance()->prepare($sql);
+    $rs->bindParam(':hood', $hoodId);
+    $rs->execute();
+  }
+  catch (PDOException $e)
+  {
+    exit(showError(500,$e));
+  }
+  while($result = $rs->fetch(PDO::FETCH_ASSOC))
+  {
+    array_push($ret, $result);
+  }
+  return $ret;
+}
+
+$hood = array();
+if (isset($_GET['lat']) && $_GET['lat']!=="" && isset($_GET['long']) && $_GET['long']!=="")
+{
+  debug("Searching a hood on ".$_GET['lat']." ".$_GET['long'].":");
+  $hood = getHoodByGeo($_GET['lat'],$_GET['long']);
+  if (!empty($hood))
+  {
+    debug($hood);
   }
 }
 
-// return either all nodes (if gateway) or all gateways (if node) from the hood
-try{
-  if (isset($result) && is_array($result) && $result['isgateway'])
-    $sql = 'SELECT * FROM nodes WHERE hood_ID=:hood;';
-  else
-    $sql = 'SELECT * FROM nodes WHERE hood_ID=:hood AND isgateway=\'1\';';
-  $rs = db::getInstance()->prepare($sql);
-  $rs->bindParam(':hood',$hood);
-  $rs->execute();
-}
-catch(PDOException $e) {
-  exit(showError(500,$e));
+if (empty($hood))
+{
+  debug("No hood found, using Trainstaion:");
+  $hood = getTrainstation();
+  debug($hood);
 }
 
-// return results in a easy parsable way
-if($rs->rowCount() > 0){
-  while($result = $rs->fetch(PDO::FETCH_ASSOC)){
-    $filename = $result['mac'];
+$json = array();
+$json['version'] = 1;
+$json['network'] = array('ula_prefix' => $hood['prefix']);
+$json['vpn'] = getAllVPNs($hood['ID']);
 
-    if($filename == INVALID_MAC || $filename == '000000000000')
-      $filename = $result['name'];
+$hood['location'] = array('lat' => $hood['lat'], 'lon' => $hood['lon']);
+unset($hood['lat']);
+unset($hood['lon']);
 
-    echo '####'.$filename.".conf\n";
-    echo '#name "'.$result['name']."\";\n";
-    echo 'key "'.$result['key']."\";\n";
-    if(preg_match("/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/",$result['ip']))
-      echo 'remote ipv4 "'.$result['ip'].'" port '.$result['port']." float;\n";
-    else
-      echo 'remote ipv6 "'.$result['ip'].'" port '.$result['port']." float;\n";
+unset($hood['ID']);
+unset($hood['prefix']);
+$json['hood'] = $hood;
 
-    echo "\n";
-  }
-  echo "###\n";
-}
+echo json_encode($json);
 
-// vim: expandtab:sw=2
+// vim: expandtab:sw=2:ts=2
 ?>
