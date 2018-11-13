cf49cd251670c3513276e3b2e8d15c4b2ceea423
Adrian Schmutzler <adrian.schmutzler@uni-bayreuth.de>
2018-11-13 16:08:08 +0100

    Show list of hoods with hood file links in hoods.php
    This reimplements the functionality of the hood.php file by
    
    hoods.php?hoodfile
    
    This updates the functionality to support polyhoods.
    
    Signed-off-by: Adrian Schmutzler <adrian.schmutzler@uni-bayreuth.de>

--- hoods.php
+++ hoods.php
@@ -2,6 +2,8 @@
 
 require "function.php";
 
+$hoodfilemode = isset($_GET['hoodfile']);
+
 $polydata = getPolyhoodsByHood(); // read polygon data for later use
 
 try {
@@ -23,12 +25,32 @@ while ( $result = $rs->fetch ( PDO::FETCH_ASSOC ) ) {
 		$hood['lat'] = floatval($result['lat']);
 		$hood['lon'] = floatval($result['lon']);
 	}
+
 	if(isset($polydata[$result['ID']])) {
 		$hood['polygons'] = array_values($polydata[$result['ID']]); // we don't need the polyids here
 	}
-	array_push($hoods, $hood);
+
+	if(!$hoodfilemode) {
+		array_push($hoods, $hood);
+	} else {
+		$ispoly = false;
+		if(isset($hood['polygons'])) {
+			$sumlat = 0;
+			$sumlon = 0;
+			// calculate average coordinates of first polygon; this may give wrong coordinates, but this is relatively unlikely
+			foreach($hood['polygons'][0] as $poly) {
+				$sumlat += $poly['lat'];
+				$sumlon += $poly['lon'];
+			}
+			$result['lat'] = $sumlat / count($hood['polygons'][0]);
+			$result['lon'] = $sumlon / count($hood['polygons'][0]);
+			$ispoly = true;
+		}
+		echo 'ID: '.$result['ID'].' ; Name: '.$result['name'].' ; Net: '.$result['net'].' ; lat: '.$result['lat'].' ; lon: '.$result['lon'].' ; type: '.($ispoly ? 'poly' : 'classic').' ; <a href="http://keyserver.freifunk-franken.de/v2/?lat='.$result['lat'].'&long='.$result['lon'].'">zum Hoodfile</a><br>';
+	}
 }
 
+if($hoodfilemode) { exit(); }
 header("Content-Type: application/json");
 echo json_encode($hoods, JSON_PRETTY_PRINT);
 
