e9ebe1e47367e3b4a1f4cb653aeef4f5d255b4fd
Adrian Schmutzler <adrian.schmutzler@uni-bayreuth.de>
2019-01-05 19:23:43 +0100

    Do not return exception details for database connection
    Signed-off-by: Adrian Schmutzler <adrian.schmutzler@uni-bayreuth.de>

--- function.php
+++ function.php
@@ -118,9 +118,13 @@ class db
     public static function getInstance()
     {
         if (!self::$instance) {
-            require('config.inc.php');
-            self::$instance = new PDO('mysql:host=' . $mysql_server . ';dbname=' . $mysql_db, $mysql_user, $mysql_pass);
-            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            try { // catch errors and do not show exception details
+                require('config.inc.php');
+                self::$instance = new PDO('mysql:host=' . $mysql_server . ';dbname=' . $mysql_db, $mysql_user, $mysql_pass);
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                exit(showError(500, "Error while connecting to database."));
+            }
         }
         return self::$instance;
     }
