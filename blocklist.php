a4a3ac5e55c0d8ff7f09489019dea516f598f6df
Dennis Eisold <mcules@freifunk-hassberge.de>
2019-12-26 04:34:31 +0100

    blocklist.php ruft nur die Einträge aus der Tabelle "blocked_keys" ab und gibt sie als String zurück. Beim Aufruf von blocklist.php bekommt man eine Tabelle angezeigt in der steht wann und warum welcher Key geblockt wurde Beim Aufruf von blocklist.php?cron=1 wird nur ein Text mit den gesperrten Keys zurück gegeben. Diesen Aufruf nutzt man im Cronjob um sein Sperrfile zu schreiben.
    Signed-off-by: de@itstall.de <de@itstall.de>

---
+++ blocklist.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Handle VPN-Key-Exchange request for Freifunk-Franken.
+ * fastd Blocklist
+ *
+ * @author  McUles <mcules@freifunk-hassberge.de>
+ *
+ * @license https://www.gnu.org/licenses/agpl-3.0.txt AGPL-3.0
+ */
+/* Include Database class */
+include "db.inc.php";
+/* Output variable */
+$strResult = "";
+try {
+    if (!$_REQUEST['cron']) {
+        $strResult .= "<table>";
+        $strResult .= "<tr><td>Timestamp</td><td>fastd Key</td><td>Begr&uuml;ndung</td>";
+    }
+    foreach (db::getInstance()->query("SELECT * FROM blocked_keys;") as $row) {
+        if ($_REQUEST['cron']) {
+            $strResult .= $row['fastd_key'] . "\n";
+        } else {
+            $strResult .= "<tr>";
+            $strResult .= "<td>" . $row['timestamp'] . "</td>";
+            $strResult .= "<td>" . $row['fastd_key'] . "</td>";
+            $strResult .= "<td>" . $row['block_reason'] . "</td>";
+            $strResult .= "</tr>";
+        }
+    }
+    if (!$_REQUEST['cron']) {
+        $strResult .= "</table>";
+    }
+} catch (PDOException $e) {
+    $e->getTrace();
+}
+echo $strResult;
